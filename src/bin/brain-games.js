9d466bc48553b171d287d13ef2bc33a72869b185
Konstantin Bo <borman.bonch@gmail.com>
2018-03-27 08:53:32 +0300

    brain-games v.1.0.3 (problems with import)

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,8 @@
 #!/usr/bin/nodejs
+// import { readline } from '../index';
+import readlineSync from 'readline-sync';
+
 console.log('Welcome to the Brain Games!');
+// readline();
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}`);
