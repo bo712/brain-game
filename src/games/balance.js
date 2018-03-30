8a6991031ba4f75c18cce49d0b0e0886b883f010
Konstantin Bo <borman.bonch@gmail.com>
2018-03-30 22:03:34 +0300

    test step 6

--- src/games/balance.js
+++ src/games/balance.js
@@ -1,23 +1,26 @@
 import welcome from '../index';
 
-  // return array.join('');
+// return array.join('');
 const robinHood = (array) => {
-  for (let i = 1; i < array.length - 1; i += 1) {
+  for (let i = 0; i < array.length; i += 1) {
+    array[i] = Number(array[i]);
+  }
+  for (let i = 1; i < array.length; i += 1) {
     array.sort();
     if (array[i] > array[0] + 1) {
-      array[0] = array[0] + 1;
-      array[i] = array[i] - 1;
+      array[0] += 1;
+      array[i] -= 1;
+      i = 1;
     }
   }
+  return array;
 };
 
 export const questAndAns = () => {
   const str = String(Math.round((Math.random() * 1000)));
-  const array = str.split('');
-
-
   const question = `${str}`;
-  const correctAnswer = String(sort(str));
+  const array = str.split('');
+  const correctAnswer = robinHood(array).join('');
   return [question, correctAnswer];
 };
 
